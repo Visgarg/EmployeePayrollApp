aa775e37e2e089fd3bd6eb4ca093fcf0389b36df
Vishal Garg <vishugarg10004@gmail.com>
2020-11-23 04:02:54 +0530

    [Vishal] peroforming validation for name

--- js/empPayroll.js
+++ js/empPayroll.js
@@ -5,4 +5,23 @@ salary.addEventListener('input',function()
 {
    output.textContent=salary.value;
 });
-     
+let employeePayrollObject= new EmployeePayrollData();   
+const name= document.querySelector('#name');
+const nameError= document.querySelector(".name-error");
+name.addEventListener('input',()=>
+{
+    try {
+        if(name.value=="")
+        {
+            nameError.textContent='';
+        }
+        else
+        {
+        employeePayrollObject.name= name.value;
+        nameError.textContent='';
+        }
+    } catch (e)
+    {
+        nameError.textContent=e;
+    }
+});
